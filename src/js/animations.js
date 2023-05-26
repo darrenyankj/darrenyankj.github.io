c7a705bea3f1376ceb42ce34d4acd21f8ea59297
Darren Yan <darrenyankj@gmail.com>
2023-05-26 17:19:48 +0800

    NavBar active animation on scroll

--- src/js/animations.js
+++ src/js/animations.js
@@ -22,3 +22,16 @@ for (const element of hiddenElementsAll) {
     const hiddenElements = document.querySelectorAll(".animation-hidden-" + element);
     hiddenElements.forEach((el) => observer.observe(el));
 }
+
+const activeLink = document.querySelectorAll(".nav-link");
+const section = document.querySelectorAll("section");
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    activeLink.forEach(ltx => ltx.classList.remove("active"));
+    activeLink[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
